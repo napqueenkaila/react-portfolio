0d2bb72362c9c466a6cf24ea9a1fd019e36c7a99
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-09-13 10:12:46 -0400

    edit services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -6,18 +6,24 @@ export default function Services() {
             <h2 className="section__title section__title--services">What I do</h2>
             <div className="services">
                 <div className="service">
-                    <h3>HTML & CSS</h3>
-                    <p></p>
+                    <img src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white" alt="HTML Badge"></img>
                 </div>
 
                 <div className="service">
-                    <h3>JavaScript</h3>
-                    <p></p>
+                    <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" alt="CSS Badge"></img>
                 </div>
 
                 <div className="service">
-                    <h3>React</h3>
-                    <p></p>
+                    <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript Badge"></img>
+                </div>
+                <div className="service">
+                    <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" alt="React Badge"></img>
+                </div>
+                <div className="service">
+                    <img src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="Tailwind CSS Badge"></img>
+                </div>
+                <div className="service">
+                    <img src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white" alt="React Router Badge"></img>
                 </div>
             </div>
 
